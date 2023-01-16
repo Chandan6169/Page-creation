894881c5708af629de46d6104bbee3b0bf1f8ac2
Chandan6169 <106052207+Chandan6169@users.noreply.github.com>
2023-01-16 14:48:15 +0000

    description

---
+++ app.js
@@ -0,0 +1,25 @@
+
+karnataka = 'Karnataka is a state in southwest India with Arabian Sea coastlines. The capital, Bengaluru (formerly Bangalore), is a high-tech hub known for its shopping and nightlife.';
+bengalore = 'Bengaluru (also called Bangalore) is the capital of India southern Karnataka state. The center of India high-tech industry, the city is also known for its parks and nightlife.'
+davangere = 'Davanagere is a city in the centre of the southern Indian state of Karnataka. It is the seventh largest city in the state, and the administrative headquarters of eponymous Davangere district.'
+function stateDetails() {
+    state = document.getElementById('Karnataka').innerHTML = karnataka;
+
+
+
+}
+function capitalDetails() {
+
+    capital = document.getElementById('Bangalore').innerHTML = bengalore;
+
+
+
+}
+
+function districtDetails() {
+
+    capital = document.getElementById('Davangere').innerHTML = davangere;
+
+
+
+}
